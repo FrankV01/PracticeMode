8714310ed219650230c1f680c42cb19e9e4ae1c8
Frank Villasenor <frank@theOpenSourceU.org>
2025-11-03 14:36:57 -0600

    More setup.

--- src/KumonApp.tsx
+++ src/KumonApp.tsx
@@ -1,23 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function App() {
+function KumonApp() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Does the passage go here?</p>
       </div>
-      <h1>Vite + React</h1>
+      <h1>PracticeMode</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -33,4 +26,4 @@ function App() {
   )
 }
 
-export default App
+export default KumonApp
