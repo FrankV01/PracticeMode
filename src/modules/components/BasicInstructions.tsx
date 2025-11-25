c3ccf5da342c811e69cfaf10383a37e2adc250b3
Frank Villasenor <frank@theOpenSourceU.org>
2025-11-25 11:58:56 -0600

    Evolution.

---
+++ src/modules/components/BasicInstructions.tsx
@@ -0,0 +1,13 @@
+/**
+ * Display the instructions to the user.
+ * Display the instructions to the user.
+ */
+
+
+export async function BasicInstructions() {
+  return (
+    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+      Instructions go here.
+    </p>
+  );
+}
---
+++ src/modules/components/Story.tsx
@@ -0,0 +1,13 @@
+/**
+ * The Objective is to display a story.
+ */
+
+// I don't know. Is something wrong or fucked up in me? Obviously. Come to think of it.
+
+export async function Story() {
+    return (
+        <div>
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Story</h1>
+        </div>
+    );
+}
